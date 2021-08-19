1d4ded9ae17ae5ad5a9cb8038a2c6382a900f0a0
Duc Thinh <thinhapcs@gmail.com>
2021-08-19 18:28:22 +0700

    chip delete category

--- src/pages/AdminProducts/index.js
+++ src/pages/AdminProducts/index.js
@@ -1,28 +1,35 @@
-import React, { useEffect, useState }  from 'react';
+import React, { useEffect, useState } from "react";
 import { makeStyles, createTheme } from "@material-ui/core/styles";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
-import ImageListItem from '@material-ui/core/ImageListItem';
-import AddIcon from '@material-ui/icons/Add';
-import { Button, Chip, ThemeProvider, IconButton, TextField, Dialog, InputAdornment } from "@material-ui/core";
+import ImageListItem from "@material-ui/core/ImageListItem";
+import AddIcon from "@material-ui/icons/Add";
+import {
+  Button,
+  Chip,
+  ThemeProvider,
+  IconButton,
+  TextField,
+  Dialog,
+  InputAdornment,
+} from "@material-ui/core";
 
 import {
   BrowserRouter as Router,
   Route,
   Switch,
-  NavLink
+  NavLink,
 } from "react-router-dom";
 
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex"
+    display: "flex",
   },
 }));
 
@@ -75,34 +82,48 @@ export default function AdminProducts() {
       )}
       {!hasErrors && (
         <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell style={{width: "20px"}}>#ID</TableCell>
-              <TableCell style={{width: "35px"}}>Hình</TableCell>
-              <TableCell >Tên sản phẩm</TableCell>
-              <TableCell style={{width: "50px"}}></TableCell>
-              <TableCell style={{width: "50px"}}></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {products.map((product) => (
-              <TableRow key={product.id}>
-                <TableCell align="right">{product.id}</TableCell>
-                <TableCell align="center">
-                  <img src={`../../${product.picture}/preview.jpg`} alt={product.name} style={{width: "100%"}} />
-                </TableCell>
-                <TableCell align="left">{product.name}</TableCell>
-                <TableCell align="center">
-                  <Button variant="contained" color="secondary">
-                    Delete
-                  </Button>
-                </TableCell>
-                <TableCell align="center">
-                  <Button variant="contained" color="primary" onClick={handleClickOpen}>
-                    Edit
-                  </Button>
-                    <Dialog open={open} onClose={handleClose} BackdropProps={{style: {backgroundColor: 'transparent'}}}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ width: "20px" }}>#ID</TableCell>
+                <TableCell style={{ width: "35px" }}>Hình</TableCell>
+                <TableCell>Tên sản phẩm</TableCell>
+                <TableCell style={{ width: "50px" }}></TableCell>
+                <TableCell style={{ width: "50px" }}></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {products.map((product) => (
+                <TableRow key={product.id}>
+                  <TableCell align="right">{product.id}</TableCell>
+                  <TableCell align="center">
+                    <img
+                      src={`../../${product.picture}/preview.jpg`}
+                      alt={product.name}
+                      style={{ width: "100%" }}
+                    />
+                  </TableCell>
+                  <TableCell align="left">{product.name}</TableCell>
+                  <TableCell align="center">
+                    <Button variant="contained" color="secondary">
+                      Delete
+                    </Button>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={handleClickOpen}
+                    >
+                      Edit
+                    </Button>
+                    <Dialog
+                      open={open}
+                      onClose={handleClose}
+                      BackdropProps={{
+                        style: { backgroundColor: "transparent" },
+                      }}
+                    >
                       <form style={{ padding: "20px 60px 20px 60px" }}>
                         <TextField
                           fullWidth
@@ -121,22 +142,38 @@ export default function AdminProducts() {
                           name="published-year"
                           label="Year of Publication"
                           placeholder={product.publishedYear}
-                          style={{marginBottom: '10px'}}
+                          style={{ marginBottom: "10px" }}
                         />
-                       {product.category
-                        ? product.category.map((item, index) => (                      
-                         <Chip
-                            label={item}
-                            onDelete={
-                            product.category.filter((it, ind) => ind !== index)}
-                        />                
-                        ))
-                        : null}
+                        {product.category
+                          ? product.category.map((item) => (
+                              <Chip
+                                label={item}
+                                onDelete={() => {
+                                  let temp = products;
+                                  for (let i in temp) {
+                                    if (temp[i] === product) {
+                                      temp[i].category.filter(
+                                        (cat) => cat !== item
+                                      );
+                                      break;
+                                    }
+                                  }
+                                  setProducts(temp);
+                                }}
+                              />
+                            ))
+                          : null}
                         <TextField
-                          style={{width: '30%', marginLeft: '5px'}}
+                          style={{ width: "30%", marginLeft: "5px" }}
                           name="new-tag"
                           InputProps={{
-                            endAdornment: <InputAdornment position="end"><IconButton><AddIcon/></IconButton></InputAdornment>,
+                            endAdornment: (
+                              <InputAdornment position="end">
+                                <IconButton>
+                                  <AddIcon />
+                                </IconButton>
+                              </InputAdornment>
+                            ),
                           }}
                         />
                         <ThemeProvider theme={mainTheme}>
@@ -145,19 +182,19 @@ export default function AdminProducts() {
                             variant="contained"
                             color="primary"
                             fullWidth
-                            style={{marginTop: '10px'}}
+                            style={{ marginTop: "10px" }}
                           >
                             Finalize
                           </Button>
                         </ThemeProvider>
                       </form>
-                    </Dialog> 
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+                    </Dialog>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       )}
     </div>
   );
